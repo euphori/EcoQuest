a5a02675bc715e50e4020596fa8f769e6cd1e21f
euphori <56996213+euphori@users.noreply.github.com>
2023-09-08 14:45:35 +0800

    fps counter and desert

--- box.gd
+++ box.gd
@@ -15,7 +15,7 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y -= gravity * delta
+		velocity.y -= gravity
 		move_and_slide()
 
 
